a845bece6aaac87d1f81be00242b8a37db192221
Hyrum Hansen <han23044@byui.edu>
2024-09-25 14:25:55 -0600

    all done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Welcome to the guess my number game! Guess a number between 1 and 100");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = 0;
+
+        while (guess != number)
+        {
+            Console.WriteLine("Guess the number:");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess == number)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+        }
+
     }
 }
