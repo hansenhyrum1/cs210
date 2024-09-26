2c0cd4ab3a35bcc7c895be3b4ef9496057a7ac96
Hyrum Hansen <han23044@byui.edu>
2024-09-25 20:56:07 -0600

    prep 4 commit

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int input = 1;
+        
+        while (input != 0)
+        {
+            Console.WriteLine("Enter any number, type 0 to stop");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+            numbers.Add(input);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is {sum}");
+
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The average is {average}");
+
+
+        int max = 0;
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is {max}");
     }
 }
