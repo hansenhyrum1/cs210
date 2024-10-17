68d4c107c4a28ffb562efa56fec38d24a5a39a23
Hyrum Hansen <han23044@byui.edu>
2024-10-16 18:59:36 -0600

    yes

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction one = new Fraction();
+        Console.WriteLine(one.GetFraction());
+        Console.WriteLine(one.GetDecimal());
+
+        Fraction two = new Fraction(5);
+        Console.WriteLine(two.GetFraction());
+        Console.WriteLine(two.GetDecimal());
+
+        Fraction three = new Fraction(3, 4);
+        Console.WriteLine(three.GetFraction());
+        Console.WriteLine(three.GetDecimal());
+
+        Fraction four = new Fraction(1, 3);
+        Console.WriteLine(four.GetFraction());
+        Console.WriteLine(four.GetDecimal());
     }
 }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -7,3 +7,52 @@ class Program
         Console.WriteLine("Hello Develop02 World!");
     }
 }
+ 
+class Entry
+{
+    public string _prompt;
+    public string _response;
+    public string _date;
+
+    public Entry(string prompt, string response)
+    {
+        _prompt = prompt;
+        _response = response;
+        _date =  DateTime.Now.ToString(); 
+    }   
+    
+    
+}
+
+class Journal
+{
+     public List<Entry> journalEntries = new List<Entry>();
+
+     public void addEntry(Entry entry)
+     {
+
+     }
+     public void showEntries()
+     {
+
+     }
+     public void save(string fileName)
+     {
+
+     }
+     public void load (string fileName)
+     {
+
+     }
+
+}
+
+class Prompts
+{
+    public List<string>;
+    
+    public void randPrompt()
+    {
+
+    }
+}
