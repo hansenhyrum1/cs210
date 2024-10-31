db18a2c8381efca93adc66a2d1743d217ed23481
hansenhyrum1 <han23044@byui.edu>
2024-10-30 22:40:45 -0600

    I hope I understand this completely

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment first = new Assignment("Hyrum", "Algebra");
+        Console.WriteLine(first.GetAll());
+
+        MathAssignment second = new MathAssignment("Hyrum", "Trigonometry", "15.4", "12-22");
+        Console.WriteLine(second.GetAll());
+        Console.WriteLine(second.GetHomeworkList());
+
+        WritingAssignment third = new WritingAssignment("Hyrum", "Why Writing is the Worst", "The Slow Death of a Student");
+        Console.WriteLine(third.GetAll());
+        Console.WriteLine(third.GetWritingInformation());
     }
 }
